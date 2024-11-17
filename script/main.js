e5c24bff5c757d5b8337cc0c74b9f80992995325
bukay2502 <127114207+bukay2502@users.noreply.github.com>
2024-11-17 23:44:07 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -127,7 +127,7 @@ const animationTimeline = () => {
     })
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=3")
     .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=3")
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
     .from(
         ".idea-5",
         0.7, {
